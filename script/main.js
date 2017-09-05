6ce6602c941f3440f2acb33c3823d98c43249fc0
Ric Lavers <riclavers@rics-mbp-2.ait-sydney.net.au>
2017-09-05 19:47:46 +1000

    evening commit

--- script/main.js
+++ script/main.js
@@ -1,20 +1,51 @@
-
 $(document).ready(function(){
+  $('.my-button').click(function(e) {
+    var str = $( this ).text();
+    $( "#inner" ).text( str );
+    console.log('working');
 
-  $('#btn-one').click(function(e) {
-    console.log('fucking working');
-
-    e.preventDefault();
-
+    // e.preventDefault();
+    $(".focus").removeClass('focus');
     $(this).addClass('focus');
 
-    // $('#one').delay(4000).addClass('open-para');
-
+    $(".open-para").removeClass("open-para");
+//a setTimeout has to be used as  delay() can only be used on animations
     setTimeout(function(){
-       $("#one").addClass("open-para");
+       $('#' + str).addClass("open-para")
+      //  $("#one").addClass("open-para");
      }, 100);
 
     //$('#one').attr('class', '')
   });
-
 });
+
+// $(document).ready(function(){
+//   $('#btn-two').click(function(e) {
+//     console.log('working');
+//
+//     // e.preventDefault();
+//     $(".focus").removeClass('focus');
+//     $(this).addClass('focus');
+//
+//     $(".open-para").removeClass("open-para");
+// //a setTimeout has to be used as  delay() can only be used on animations
+//     setTimeout(function(){
+//        $("#two").addClass("open-para");
+//      }, 100);
+//
+//     //$('#one').attr('class', '')
+//   });
+// });
+
+
+
+// $(document).ready(function(){
+//   $("#btn-one").click(function(e){
+//     e.preventDefault();
+//     $(this).addClass('focus');
+//   });
+// });
+//
+// $("#btn-one").click(function() {
+//   $("open-para").toggle(100);
+// });
